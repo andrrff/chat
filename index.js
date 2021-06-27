3978562cbc655cce4c4963059628854c81d0f908
Suco de Fruta <andrecastanhal@gmail.com>
2021-06-27 19:58:22 -0300

    hotfix erros

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || "0.0.0.0";
 
 app.listen(server_port, server_host, function () {
-    log.info("Listening on port %d", server_port);
+    console.log("Listening on port %d", server_port);
 });
