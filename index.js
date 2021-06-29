730edbc89fad682999a4bd019e9cc81da0666699
Suco de Fruta <andrecastanhal@gmail.com>
2021-06-29 19:55:44 -0300

    add popup username

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const express = require("express"),
+    app = express(),
+    http = require("http"),
+    server = http.createServer(app),
+    { Server } = require("socket.io"),
+    io = new Server(server);
 
+//Json.stringify()
+var usocket = {},
+    user = [];
 var users = 0;
 
-app.get('/', (_req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (_req, res) => {
+    res.sendFile(__dirname + "/view/index.html");
 });
 
-io.on('connection', (socket) => {
-  users++;
-  console.log('User active: ' + users);
-  socket.on('chat message', (msg, user) =>{
-    io.emit("chat message", msg, user);
-    console.log('user: '+ user + ' -> ' + msg);
-  });
-  socket.on('disconnect', () => {
-      console.log('A user disconnected')
-      users--;
-      console.log("User active: " + users);
-  })
+io.on("connection", (socket) => {
+    users++;
+    console.log("User active: " + users);
+    socket.on("chat message", (msg, user) => {
+        io.emit("chat message", msg, user);
+        console.log("user: " + user + " -> " + msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+        users--;
+        console.log("User active: " + users);
+    });
+
+    socket.on("send private message", function (res) {
+        console.log(res);
+        if (res.recipient in usocket) {
+            usocket[res.recipient].emit("receive private message", res);
+        }
+    });
 });
 
-app.set('port', (process.env.PORT || 3000));
+app.set("port", process.env.PORT || 3000);
 app.use(express.static("public"));
 
-server.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
+server.listen(app.get("port"), function () {
+    console.log("Node app is running on port", app.get("port"));
 });
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,3 +1,4 @@
+// const Swal = require('sweetalert2');
 var socket = io();
 
 var messages = document.getElementById("messages");
@@ -5,18 +6,33 @@ var form = document.getElementById("form");
 var input = document.getElementById("input");
 var username = document.getElementById("username");
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    if (input.value && username.value) {
-        socket.emit("chat message", input.value, username.value);
-        username.value = username.value;
-        input.value = "";
-    }
-});
+(async () => {
+    const { value: formValues } = await Swal.fire({
+        title: "Welcome",
+        html:
+            '<input id="swal-input1" type="username" placeholder="username" class="swal2-input">',
+        focusConfirm: true,
+        preConfirm: () => {
+            return [
+                document.getElementById("swal-input1").value,
+            ];
+        },
+    });
+    console.log("Bem-vindo " + formValues[0] + ", seja educado com os amiguinhos😊");
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        if (input.value && formValues) {
+            socket.emit("chat message", input.value, formValues[0]);
+            input.value = "";
+        }
+    });
+    
+    socket.on("chat message", (msg, user) => {
+        var item = document.createElement("li");
+        item.textContent = user + ": " + msg;
+        messages.appendChild(item);
+        window.scrollTo(0, document.body.scrollHeight);
+    });
+})();
 
-socket.on("chat message", (msg, user) => {
-    var item = document.createElement("li");
-    item.textContent = user + ": " + msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
-});
+
